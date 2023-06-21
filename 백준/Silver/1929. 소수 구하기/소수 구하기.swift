5e22621bc1b3656684c14404ebf19f01d3c2824a
김용재(Miro) <85781941+longlivedrgn@users.noreply.github.com>
2023-06-21 15:30:09 +0900

    [Silver III] Title: 소수 구하기, Time: 76 ms, Memory: 80472 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.swift
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.swift
@@ -1,15 +1,15 @@
 import Foundation
 
-let input = readLine()!.split(separator: " ").map { Int($0)! }
-let m = input[0]
-let n = input[1]
+var nums = readLine()!.split(separator: " ").map {Int($0)!}
+let m = nums[0]
+let n = nums[1]
 
-var isPrimeNumber = [Bool](repeating: true, count: n + 1)
-isPrimeNumber[1] = false
+var isPrimeNumber = [Bool](repeating: true, count: n+1)
+isPrimeNumber[1] = false // 1은 소수가 될 수 없으니까!
 
-for i in 2..<Int(sqrt(Double(n)) + 1) {
+for i in 2..<Int(sqrt(Double(n))+1) {
     if isPrimeNumber[i] {
-        var j = 2
+        var j = i
         while i * j <= n {
             isPrimeNumber[i * j] = false
             j += 1
