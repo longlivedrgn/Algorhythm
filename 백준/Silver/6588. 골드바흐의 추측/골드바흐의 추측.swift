d69acf98088df5ff57d7dca057692924e63ab12a
김용재(Miro) <85781941+longlivedrgn@users.noreply.github.com>
2023-06-22 11:44:37 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 268 ms, Memory: 70080 KB -BaekjoonHub

---
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.swift
@@ -0,0 +1,34 @@
+
+var isGoldBach = false
+var isPrimeNum = Array(repeating: true, count: 1000001)
+isPrimeNum[1] = false
+for i in 2...1000000 {
+    if isPrimeNum[i] == false { continue }
+
+    for j in stride(from: i+i, through: 1000000, by: i) {
+        isPrimeNum[j] = false
+    }
+}
+
+while true {
+    let n = Int(readLine()!)!
+
+    if n == 0 {
+        break
+    }
+
+    for m in 3..<((n/2)+1) {
+        if isPrimeNum[m] {
+            let diff = n - m
+            if isPrimeNum[diff] {
+                print("\(n) = \(m) + \(diff)")
+                isGoldBach = true
+                break
+            }
+        }
+    }
+    if !isGoldBach {
+        print("Goldbach's conjecture is wrong.")
+    }
+    isGoldBach = false
+}
