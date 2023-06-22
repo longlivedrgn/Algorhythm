4591dfab259505737c9a04b1be8bbc8f8bc4bca5
김용재(Miro) <85781941+longlivedrgn@users.noreply.github.com>
2023-06-22 11:47:22 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 244 ms, Memory: 70080 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.swift
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.swift
@@ -1,34 +1,43 @@
+private func primeNumArr() -> [Bool] {
+    var isprimenum = Array(repeating: true, count: 1000001) // 에라토스테네스 체 생성하기
+    isprimenum[0] = false
+    isprimenum[1] = false
 
-var isGoldBach = false
-var isPrimeNum = Array(repeating: true, count: 1000001)
-isPrimeNum[1] = false
-for i in 2...1000000 {
-    if isPrimeNum[i] == false { continue }
+    for i in 2..<1000001 {
+        if isprimenum[i] == false { continue } // 자기 자신은 false로 설정하기
 
-    for j in stride(from: i+i, through: 1000000, by: i) {
-        isPrimeNum[j] = false
-    }
-}
-
-while true {
-    let n = Int(readLine()!)!
-
-    if n == 0 {
-        break
-    }
-
-    for m in 3..<((n/2)+1) {
-        if isPrimeNum[m] {
-            let diff = n - m
-            if isPrimeNum[diff] {
-                print("\(n) = \(m) + \(diff)")
-                isGoldBach = true
-                break
-            }
+        for j in stride(from: i+i, through: 1000000, by: i) { // 그리고 계속해서 i를 더해주면서 false로 변경하기
+            isprimenum[j] = false
         }
     }
-    if !isGoldBach {
-        print("Goldbach's conjecture is wrong.")
-    }
-    isGoldBach = false
+    return isprimenum
 }
+private func solution() {
+    let isprime = primeNumArr() // 일단 0~ 1000001 까지의 소수를 싹 다 true로 해놓은 배열 만들기
+
+    for _ in 0..<100000 {
+        var flag = false
+        let n = Int(readLine()!)!
+
+        if n == 0 { // 0을 만나면 프로그램 종료!
+            break
+        } else {
+            for i in 3...n {
+                if isprime[i] {
+                    let diff = n - i
+                    if isprime[diff] {
+                        flag = true
+                        print("\(n) = \(i) + \(diff)")
+                        break
+                    }
+                }
+            }
+        }
+
+        if !flag {
+            print("Goldbach's conjecture is wrong.")
+        }
+    }
+}
+
+solution()
