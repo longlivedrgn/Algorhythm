a2a35fef94a2c0731844f5f590d74b3d119f0ac6
김용재(Miro) <85781941+longlivedrgn@users.noreply.github.com>
2023-06-24 15:32:30 +0900

    [Silver III] Title: 1로 만들기, Time: 56 ms, Memory: 87324 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+var num = Int(readLine()!)!
+var dp = Array(repeating: 0, count: num+1)
+
+for i in 2..<num+1 {
+    dp[i] = dp[i-1] + 1
+    if i % 3 == 0 {
+        dp[i] = min(dp[i], dp[i/3]+1)
+    }
+    if i % 2 == 0 {
+        dp[i] = min(dp[i], dp[i/2]+1)
+    }
+}
+
+print(dp[num])
