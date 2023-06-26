ca77ceb81287ead5d9406bfd70368214ca67137f
김용재(Miro) <85781941+longlivedrgn@users.noreply.github.com>
2023-06-26 12:36:32 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.swift
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.swift
@@ -3,11 +3,11 @@ import Foundation
 var num = Int(readLine()!)!
 var dp = Array(repeating: 0, count: 11)
 
-dp[0] = 1
 dp[1] = 1
 dp[2] = 2
+dp[3] = 4
 
-for i in 3...10 {
+for i in 4...10 {
     dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
 }
 for _ in 0..<num {
