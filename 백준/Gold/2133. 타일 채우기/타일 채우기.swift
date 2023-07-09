c223132b8f5a23880ec2b571b4f78926a8aec2c0
Yongjae Kim(Miro) <85781941+longlivedrgn@users.noreply.github.com>
2023-07-09 12:42:41 +0900

    [Gold IV] Title: 타일 채우기, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.swift
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.swift
@@ -1,20 +1,17 @@
-
 var input = Int(readLine()!)!
 var dp = Array(repeating: 0, count: 31)
 dp[2] = 3
 dp[4] = 11
 
 if input > 5 {
-    for k in stride(from: 6, through: input, by: 2) {
-        for m in stride(from: k-4, through: 2, by: -2) {
-            dp[k] += dp[m] * 2
+    for k in 6...input {
+        if k % 2 == 0 {
+            for m in stride(from: k-4, through: 2, by: -2) {
+                dp[k] += dp[m] * 2
+            }
+            dp[k] += dp[k-2]*dp[2] + 2
         }
-        dp[k] += dp[k-2]*dp[2] + 2
     }
 }
 
-if input % 2 == 1 {
-    print(0)
-} else {
-    print(dp[input])
-}
+print(dp[input])
