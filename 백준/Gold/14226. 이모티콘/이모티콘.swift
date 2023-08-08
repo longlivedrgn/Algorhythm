bfde000fe577e821614d046848bfe74f28064469
Yongjae Kim(Miro) <85781941+longlivedrgn@users.noreply.github.com>
2023-08-08 18:03:23 +0900

    [Gold IV] Title: 이모티콘, Time: 56 ms, Memory: 73212 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.swift
+++ 백준/Gold/14226. 이모티콘/이모티콘.swift
@@ -1,39 +1,34 @@
-let num = Int(String(readLine()!))!
-var visited = Array(repeating: Array(repeating: false, count: 2000), count: 2000)
-// 현재 바탕화면, 클립보드, 초(소요시간)
+let n = Int(String(readLine()!))!
+//화면, 클립보드, 시간
 var queue: [(Int, Int, Int)] = []
+var visited = Array(repeating: Array(repeating: false, count: 2000), count: 2000)
 
 visited[1][0] = true
-func bfs(_ start: (Int, Int, Int)){
-    queue.append(start)
-    while !queue.isEmpty{
-        let popped = queue.removeFirst()
-        if popped.0 == num {
-            print(popped.2)
-            break
-        }
-        if popped.0 >= 1 && popped.0 < 2000{
-            if !visited[popped.0][popped.0]{
-                visited[popped.0][popped.0] = true
-                queue.append((popped.0,popped.0,popped.2 + 1))
-            }
-
-            if !visited[popped.0 - 1][popped.1]{
-                visited[popped.0 - 1][popped.1] = true
-                queue.append((popped.0 - 1, popped.1, popped.2 + 1))
-            }
+queue.append((1, 0, 0))
+while !queue.isEmpty{
+    let pop = queue.removeFirst()
+    if pop.0 == n {
+        print(pop.2)
+        break
+    }
+    if pop.0 >= 1 && pop.0 < 2000{
+        if !visited[pop.0][pop.0]{
+            visited[pop.0][pop.0] = true
+            queue.append((pop.0,pop.0,pop.2 + 1))
         }
 
-        if popped.1 > 0 && popped.0 + popped.1 < 2000{
-            if !visited[popped.0 + popped.1][popped.1]{
-                visited[popped.0 + popped.1][popped.1] = true
-                queue.append((popped.0 + popped.1, popped.1, popped.2 + 1))
-            }
+        if !visited[pop.0 - 1][pop.1]{
+            visited[pop.0 - 1][pop.1] = true
+            queue.append((pop.0 - 1, pop.1, pop.2 + 1))
         }
+    }
 
-
+    if pop.1 > 0 && pop.0 + pop.1 < 2000{
+        if !visited[pop.0 + pop.1][pop.1]{
+            visited[pop.0 + pop.1][pop.1] = true
+            queue.append((pop.0 + pop.1, pop.1, pop.2 + 1))
+        }
     }
 
 
 }
-bfs((1, 0, 0))
