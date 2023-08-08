c84dad907bde0f82396ef4ee8d2314b1c0afb86a
Yongjae Kim(Miro) <85781941+longlivedrgn@users.noreply.github.com>
2023-08-08 18:10:52 +0900

    [Gold IV] Title: 이모티콘, Time: 20 ms, Memory: 83756 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.swift
+++ 백준/Gold/14226. 이모티콘/이모티콘.swift
@@ -1,34 +1,75 @@
-let n = Int(String(readLine()!))!
-//화면, 클립보드, 시간
-var queue: [(Int, Int, Int)] = []
-var visited = Array(repeating: Array(repeating: false, count: 2000), count: 2000)
+import Foundation
 
+struct Queue<T> {
+    var inputStack = [T]()
+    var outputStack = [T]()
+
+    mutating func append(_ x: T) {
+        inputStack.append(x)
+    }
+
+    mutating func pop() -> T? {
+        var top: T?
+        if outputStack.isEmpty {
+            outputStack = inputStack.reversed()
+            inputStack.removeAll()
+            top = outputStack.popLast()
+        }
+        else {
+            top = outputStack.popLast()
+        }
+        return top
+    }
+
+    mutating func head() -> T? {
+        if outputStack.isEmpty {
+            outputStack = inputStack.reversed()
+            inputStack.removeAll()
+        }
+        return outputStack.isEmpty ? nil : outputStack[outputStack.endIndex-1]
+    }
+
+    func isEmpty() -> Bool {
+        return inputStack.isEmpty && outputStack.isEmpty ? true : false
+    }
+}
+
+
+var num = Int(readLine()!)!
+var visited = Array(repeating: Array(repeating: false, count: 2001), count: 2001)
+// 현재 바탕화면, 클립보드, 초(소요시간)
+var queue = Queue<(Int, Int, Int)>()
+
+// 처음 시작할 때 바탕화면에 한 개의 임티는 존재
+// 따라서 바탕하면에는 한 개, 클립보드에는 0개는 방문했다고 가정!
 visited[1][0] = true
-queue.append((1, 0, 0))
-while !queue.isEmpty{
-    let pop = queue.removeFirst()
-    if pop.0 == n {
-        print(pop.2)
+
+queue.append((1,0,0))
+while !queue.isEmpty() {
+    let popped = queue.pop()!
+    if popped.0 == num {
+        print(popped.2)
         break
     }
-    if pop.0 >= 1 && pop.0 < 2000{
-        if !visited[pop.0][pop.0]{
-            visited[pop.0][pop.0] = true
-            queue.append((pop.0,pop.0,pop.2 + 1))
-        }
 
-        if !visited[pop.0 - 1][pop.1]{
-            visited[pop.0 - 1][pop.1] = true
-            queue.append((pop.0 - 1, pop.1, pop.2 + 1))
+    if popped.0 > 0 {
+        // 바탕화면에서 하나 빼기
+        if !visited[popped.0-1][popped.1] {
+            visited[popped.0-1][popped.1] = true
+            queue.append((popped.0-1, popped.1, popped.2+1))
         }
     }
 
-    if pop.1 > 0 && pop.0 + pop.1 < 2000{
-        if !visited[pop.0 + pop.1][pop.1]{
-            visited[pop.0 + pop.1][pop.1] = true
-            queue.append((pop.0 + pop.1, pop.1, pop.2 + 1))
-        }
+    // 바탕화면에 있는 이모티콘 클립보드로 복사하기
+    if !visited[popped.0][popped.0] {
+        visited[popped.0][popped.0] = true
+        queue.append((popped.0, popped.0, popped.2+1))
     }
 
-
+    if popped.1 > 0 && popped.0+popped.1 <= 2000 {
+        if !visited[popped.0 + popped.1][popped.1] {
+            visited[popped.0 + popped.1][popped.1] = true
+            queue.append((popped.0 + popped.1, popped.1, popped.2 + 1))
+        }
+    }
 }
