6eb468bb0954c8c3dfdc4b2fd0ad1c99ceb373db
Yongjae Kim(Miro) <85781941+longlivedrgn@users.noreply.github.com>
2023-08-17 12:37:57 +0900

    [Bronze V] Title: 사칙연산, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.swift
@@ -0,0 +1,7 @@
+var nums = readLine()!.split(separator: " ").map {Int(String($0))!}
+
+print(nums[0] + nums[1])
+print(nums[0] - nums[1])
+print(nums[0] * nums[1])
+print(nums[0] / nums[1])
+print(nums[0] % nums[1])
