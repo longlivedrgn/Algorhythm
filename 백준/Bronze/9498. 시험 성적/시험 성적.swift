aa7b4a415e290d05dc3be6f3568ad633a26fe980
Yongjae Kim(Miro) <85781941+longlivedrgn@users.noreply.github.com>
2023-09-02 16:51:33 +0900

    [Bronze V] Title: 시험 성적, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.swift
@@ -0,0 +1,12 @@
+var num = Int(readLine()!)!
+if num >= 90 && num <= 100 {
+    print("A")
+} else if num >= 80 && num <= 89 {
+    print("B")
+} else if num >= 70 && num <= 79 {
+    print("C")
+} else if num >= 60 && num <= 69 {
+    print("D")
+} else {
+    print("F")
+}
