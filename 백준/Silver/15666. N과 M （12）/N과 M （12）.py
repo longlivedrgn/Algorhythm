587465c9c69fc6c4a3841c9bb9e972b92915c6a9
Yongjae Kim(Miro) <85781941+longlivedrgn@users.noreply.github.com>
2023-12-26 21:35:57 +0900

    [Silver II] Title: N과 M (12), Time: 216 ms, Memory: 112920 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+nums.sort()
+
+lst = []
+visited = [False] * n
+numSet = set()
+
+def DFS(depth):
+    if len(lst) == m:
+        temp = " ".join(map(str, lst))
+        if temp not in numSet:
+            print(temp)
+            numSet.add(temp)
+        return
+    for i in range(depth, n):
+        lst.append(nums[i])
+        visited[i] = True
+        DFS(i)
+        lst.pop()
+        visited[i] = False
+DFS(0)
