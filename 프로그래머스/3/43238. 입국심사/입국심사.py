5d84aff5d0f69962133bccf6ee77462abfe5a28f
Yongjae Kim(Miro) <85781941+longlivedrgn@users.noreply.github.com>
2024-01-05 14:34:24 +0900

    [level 3] Title: 입국심사, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,16 +1,38 @@
+# def solution(n, times):
+#     answer = 0
+
+#     left, right = 1, max(times) * n # left는 제일 작게, right는 최악의 경우를 가정하기
+#     while left <= right:
+#         mid = (left + right) // 2
+#         peopleNumber = 0
+#         for time in times:
+#             peopleNumber += (mid // time)
+#         if peopleNumber <= n:
+#             left = mid + 1
+            
+#             answer = mid
+#         else:
+#             right = mid - 1
+
+#     return answer
 def solution(n, times):
     answer = 0
+    end = n * max(times)
+    start = 0
 
-    left, right = 1, max(times) * n # left는 제일 작게, right는 최악의 경우를 가정하기
-    while left <= right:
-        mid = (left + right) // 2
-        peopleNumber = 0
+    while start <= end:
+        mid = (start + end) // 2
+        people = 0
+
         for time in times:
-            peopleNumber += (mid // time)
-        if peopleNumber >= n:
-            right = mid - 1
-            answer = mid
+            people += (mid // time)
+
+        if people < n:
+            start = mid + 1
+            
         else:
-            left = mid + 1
+            end = mid - 1
+            answer = mid
+
 
     return answer
