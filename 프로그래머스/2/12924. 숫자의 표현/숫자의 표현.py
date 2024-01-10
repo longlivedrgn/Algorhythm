5eb96915176e7af7f3314a0fcfca074255729937
Yongjae Kim <85781941+longlivedrgn@users.noreply.github.com>
2024-01-10 13:58:07 +0900

    [level 2] Title: 숫자의 표현, Time: 5.14 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,14 +1,9 @@
-
 def solution(n):
-    answer =  0
-    
-    for i in range(1, n+1):
-        sum = 0
-        for j in range(i, n+1):
-            sum += j
-            if sum == n:
-                answer +=1
-                break
-            elif sum > n:
-                break
-    return  answer
+    numbers = [i for i in range(1, n+1)]
+    count = 0
+    for i in range(n):
+        for j in range(i+1, n):
+            if sum(numbers[i:j+1]) == n:
+                count += 1
+
+    return count+1
