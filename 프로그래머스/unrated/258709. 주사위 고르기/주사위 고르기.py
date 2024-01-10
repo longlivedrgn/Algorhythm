7a9c1c8b68e7a08aa6dc0c9857f536bbb7870ef8
Yongjae Kim <85781941+longlivedrgn@users.noreply.github.com>
2024-01-10 11:56:48 +0900

    [level unrated] Title: 주사위 고르기, Time: 1472.55 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/258709. 주사위 고르기/주사위 고르기.py
@@ -0,0 +1,34 @@
+import itertools
+from itertools import product
+import bisect
+
+def solution(dice):
+    dice_number = len(dice)
+    pick_list = list(itertools.combinations([i for i in range(dice_number)], dice_number // 2))
+
+    def calculate_sum_combinations(arr, indices):
+        combinations = product(*(arr[idx] for idx in indices))
+        result = [sum(combo) for combo in combinations]
+        return result
+
+    answer = 0
+    number = 0
+    for pick in pick_list: # (0,1)
+        sum_A  = calculate_sum_combinations(dice, pick)
+        tempPickList = [i for i in range(dice_number)]
+        b_pick = [x for x in tempPickList if x not in pick]
+        sum_B = calculate_sum_combinations(dice, b_pick)
+        sum_A.sort()
+        sum_B.sort()
+        tempCount = 0
+        j = 0
+        for num1 in sum_A:
+            while j < len(sum_B) and num1 > sum_B[j]:
+                j += 1
+            tempCount += j
+
+        if tempCount > number:
+            answer = pick
+            number = tempCount
+
+    return list(map(lambda x: x+ 1, answer))
