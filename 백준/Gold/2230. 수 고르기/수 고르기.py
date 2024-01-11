6dac06a581b28f8b03c7a89c41d64a0a8aef1873
Yongjae Kim <85781941+longlivedrgn@users.noreply.github.com>
2024-01-11 11:01:48 +0900

    [Gold V] Title: 수 고르기, Time: 7324 ms, Memory: 114916 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.py
+++ 백준/Gold/2230. 수 고르기/수 고르기.py
@@ -1,11 +1,10 @@
-import math
 n, m = map(int, input().split())
 numbers = []
 for _ in range(n):
     numbers.append(int(input()))
 
 numbers.sort()
-answer = math.inf
+answer = 10987654321
 for i in range(n):
     for j in range(i, n):
         gap = numbers[j] - numbers[i]
