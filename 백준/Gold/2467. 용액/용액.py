934c03f95080dd53a881686a6b2e2bdeffae075f
Yongjae Kim <85781941+longlivedrgn@users.noreply.github.com>
2024-03-19 19:41:52 +0900

    [Gold V] Title: 용액, Time: 160 ms, Memory: 124044 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -1,32 +1,28 @@
-n = int(input())
+N = int(input())
 liquids = list(map(int, input().split()))
+answer = float("INF")
+left = 0
+right = 0
 
-ans = float("INF")
-ans_left = 0
-ans_right = 0
-
-for i in range(n-1):
+for i in range(N-1): # index 0부터 index 마지막 보다 한 칸 앞까지 싹 다 순회를 한다.
     current = liquids[i]
+    temp_left = i+1
+    temp_right = N-1
 
-    start = i + 1
-    end = n - 1
+    while temp_left <= temp_right:
+        mid = (temp_left + temp_right) // 2
+        temp_sum = liquids[mid] + current
 
-    while start <= end:
-        mid = (start + end) // 2
-        tmp = current + liquids[mid]
+        if abs(temp_sum) < answer:
+            answer = abs(temp_sum)
+            left = i
+            right = mid
 
-        if abs(tmp) < ans:
-            ans = abs(tmp)
-            ans_left = i
-            ans_right = mid
-
-            if tmp == 0:
+            if temp_sum == 0:
                 break
-        
-        if tmp < 0:
-            start = mid + 1
-        
+        if temp_sum < 0:
+            temp_left = mid + 1
         else:
-            end = mid - 1
+            temp_right = mid - 1
 
-print(liquids[ans_left], liquids[ans_right])
+print(liquids[left], liquids[right])
