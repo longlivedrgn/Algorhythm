f54bf774df9c3ef8da12d823e3d09a20a42309a5
Yongjae Kim <85781941+longlivedrgn@users.noreply.github.com>
2024-05-20 10:49:19 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 499.83 ms, Memory: 173 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
@@ -6,11 +6,9 @@ def solution(numbers):
             temp.append((idx, n))
             continue
         else:
-            while n > temp[-1][1]:
+            while temp and n > temp[-1][1]:
                 answer[temp[-1][0]] = n
                 temp.pop()
-                if len(temp) == 0:
-                    break
             temp.append((idx, n))
     for t in temp:
         answer[t[0]] = -1
