d1502a135197b95d06cf17c2a9c26cc61275c618
Yongjae Kim <85781941+longlivedrgn@users.noreply.github.com>
2024-05-24 16:29:29 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/README.md
+++ 프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.01 ms
+메모리: 10.2 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 3일 17:24:0
+2024년 05월 24일 16:29:23
 
 ### 문제 설명
 
--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,36 +1,22 @@
-# def solution(tickets):
-#     answer = []
-#     visited = [False]*len(tickets)
-
-#     def DFS(airport, path):
-#         if len(path) == len(tickets)+1: # 만약 Ticket을 전체 다 사용했다면?..
-#             answer.append(path) # 마지막 Path를 append하고 끝낸다!..
-#             return
-#         for idx, ticket in enumerate(tickets):
-#             if airport == ticket[0] and visited[idx] == False:
-#                 visited[idx] = True
-#                 DFS(ticket[1], path+[ticket[1]])
-#                 visited[idx] = False
-
-#     DFS("ICN", ["ICN"])
-#     answer.sort() # 일단 되는 모든 얘들을 answer에 append하므로, 거기서 sorting을 해서 결정한다.
-#     return answer[0]
-
 def solution(tickets):
-    answer = []
-    visited = [False]*len(tickets)
+    visited = [False for i in range(len(tickets))]
+    answers = []
 
-    def DFS(airport, path):
-        if len(path) == len(tickets) + 1:
-            answer.append(path)
+    def DFS(start, path): # 2, [ICN, JFK, "HND"]
+        if len(path) == len(tickets)+1:
+            answers.append(path)
             return
-        for i in range(len(tickets)):
-            if airport == tickets[i][0] and visited[i] == False:
-                visited[i] = True
-                DFS(tickets[i][1], path+[tickets[i][1]])
-                visited[i] = False
+        for (idx, ticket) in enumerate(tickets):
+            if visited[idx] == False and ticket[0] == tickets[start][1]:
+                visited[idx] = True
+                DFS(idx, path + [ticket[1]])
+                visited[idx] = False
 
-    DFS("ICN", ['ICN'])
-    answer.sort()
-    return answer[0]
-
+    for (idx,ticket) in enumerate(tickets):
+        if ticket[0] == "ICN":
+            visited[idx] = True
+            DFS(idx, ticket) # [ICN, JFK]
+            visited[idx] = False
+    answers.sort()
+    
+    return answers[0]
