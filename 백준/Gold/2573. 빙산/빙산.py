d2685031b901dbe7baca11244c17565506f7bf07
Yongjae Kim <85781941+longlivedrgn@users.noreply.github.com>
2024-05-24 15:05:39 +0900

    [Gold IV] Title: 빙산, Time: 756 ms, Memory: 208068 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,74 +1,72 @@
 from collections import deque
-
-inputs = list(map(int, input().split()))
-row = inputs[0]
-column = inputs[1]
-
-graph = []
-for _ in range(row):
-    graph.append(list(map(int, input().split())))
+answer = 0
+R, C = map(int, input().split())
+board = []
+for _ in range(R):
+    board.append(list(map(int, input().split())))
 
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
 
-year = -1
+def BFS(start):
+    queue = deque()
+    queue.append(start)
+
+    while queue:
+        popped = queue.popleft()
+
+        for i in range(4):
+            n_c = popped[1] + dc[i]
+            n_r = popped[0] + dr[i]
+
+            if 0 <= n_c < C and 0 <= n_r < R and board[n_r][n_c] != 0 and visited[n_r][n_c] == False:
+                visited[n_r][n_c] = True
+                queue.append((n_r, n_c))
 
 
 while True:
-    year += 1
-
-    visited = [[False for _ in range(column)] for _ in range(row)]
-
-    def BFS(start):
-        queue = deque()
-        queue.append(start)
-
-        while queue:
-            r, c = queue.popleft()
-
-            for i in range(4):
-                next_r = r + dr[i]
-                next_c = c + dc[i]
-
-                if 0 <= next_r < row and 0 <= next_c < column:
-                    if graph[next_r][next_c] != 0 and visited[next_r][next_c] == False:
-                        queue.append([next_r, next_c])
-                        visited[next_r][next_c] = True
-
-    island = 0
-    for r in range(row):
-        for c in range(column):
-            if graph[r][c] != 0 and visited[r][c] == False:
-                island += 1
-                BFS([r, c])
-
-    if island >= 2:
-        print(year)
-        break
-    if island == 0:
-        print(0)
-        break
-
-    # 싹 다 돌아서 어느 좌표에서 녹는 지를 담아두고, 한번에 다 녹여버린다.
-    # 그리고, 2개로 나누어져있는 지 판단하기
-    melting_list = []
-
-    # melting_list 채우기
-    for r in range(row):
-        for c in range(column):
-            if graph[r][c] != 0:
-                # 주변에 0이 몇 개인지를 확인한다.
-                count = 0
-
+    melting_point = []
+    for c in range(C):
+        for r in range(R):
+            if board[r][c] != 0: # 얼음이 존재한다.
+                zero_count = 0
                 for i in range(4):
-                    next_r = r + dr[i]
-                    next_c = c + dc[i]
+                    n_r = r + dr[i]
+                    n_c = c + dc[i]
 
-                    if 0 <= next_r < row and 0 <= next_c < column:
-                        if graph[next_r][next_c] == 0:
-                            count += 1
-                melting_list.append([r, c, count])
+                    if 0 <= n_c < C and 0 <= n_r < R:
+                        if board[n_r][n_c] == 0:
+                            zero_count += 1
+                melting_point.append((r, c, zero_count))
 
-    # melting_list에 있는 거 싹 다 지우기
-    for melt in melting_list:
-        graph[melt[0]][melt[1]] = max(0, graph[melt[0]][melt[1]] - melt[2])
+    for melt in melting_point:
+        r = melt[0]
+        c = melt[1]
+
+        board[r][c] -= melt[2]
+
+        if board[r][c] < 0:
+            board[r][c] = 0
+
+    answer += 1
+
+    # 빙산의 갯수를 찾자!...
+    visited = [[False for _ in range(C)] for _ in range(R)]
+    island_count = 0
+    for c in range(C):
+        for r in range(R):
+            if board[r][c] != 0 and visited[r][c] == False:
+                island_count += 1
+                visited[r][c] = True
+                BFS((r,c))
+
+    if island_count == 0:
+        answer = 0
+        break
+    if island_count >= 2:
+        break
+print(answer)
+
+
+
+
