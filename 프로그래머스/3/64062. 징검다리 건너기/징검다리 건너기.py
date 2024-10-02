bc28d878ecf72b478b66dc13b31bf191c5ad80d4
Yongjae Kim <85781941+longlivedrgn@users.noreply.github.com>
2024-10-02 21:11:03 +0900

    [level 3] Title: 징검다리 건너기, Time: 399.50 ms, Memory: 18.5 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -1,9 +1,11 @@
 def solution(stones, k):
+    answer = 0
     start = 1
     end = max(stones)
     while start <= end:
         cnt = 0 # 연속해서 0인 디딤돌 갯수
         mid = (start + end) // 2
+
         for stone in stones:
             if (stone - mid) <= 0:
                 cnt += 1
@@ -13,5 +15,6 @@ def solution(stones, k):
             else:
                 cnt = 0 # 만약 건널 수 있는 게 생기면 cnt를 다시 초기화
         else: #만약 for문을 다 돌았다면?..
-            start = mid + 1
+            start = mid + 1 # 최소를 구해야하므로 start가 답이된다!..
+            answer = mid + 1
     return start
