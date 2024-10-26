b30d5fed422841a37a94c4869cb9351ea11d5aff
Yongjae Kim <85781941+longlivedrgn@users.noreply.github.com>
2024-10-26 15:20:40 +0900

    [level 3] Title: 스티커 모으기(2), Time: 74.97 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -1,19 +1,21 @@
 def solution(sticker):
-    # 첫번째 꺼를 선택한 경우 / 첫번째 선택을 안한 경우
     if len(sticker) == 1:
         return sticker[0]
+    # 첫번째를 먹는거랑 안먹는거랑 나눠서 생각하기
     dp1 = [0 for _ in range(len(sticker))]
     dp2 = [0 for _ in range(len(sticker))]
-    dp1[0] = sticker[0] # 첫번째 선택 O
-    dp1[1] = sticker[0] # 첫번째 선택 X
     
+    # 첫번째를 먹었음
+    dp1[0] = sticker[0]
+    dp1[1] = sticker[0]
+    
+    # 두번째를 먹었음
     dp2[0] = 0
     dp2[1] = sticker[1]
     
-    for i in range(2, len(sticker)-1):
+    for i in range(2, len(sticker) -1):
         dp1[i] = max(dp1[i-2]+sticker[i], dp1[i-1])
-        
-    for i in range(2, len(sticker)): 
+    
+    for i in range(2, len(sticker)):
         dp2[i] = max(dp2[i-2]+sticker[i], dp2[i-1])
-
-    return max(max(dp1), max(dp2))
+    return max(max(dp2), max(dp1))
