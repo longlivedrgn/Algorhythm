5d60353b4688025374cd431300fc9f00e831c083
Yongjae Kim <85781941+longlivedrgn@users.noreply.github.com>
2024-10-26 18:12:24 +0900

    [Gold IV] Title: 고층 건물, Time: 96 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/1027. 고층 건물/고층 건물.py
+++ 백준/Gold/1027. 고층 건물/고층 건물.py
@@ -1,35 +1,31 @@
-building_count = int(input())
+buildings_count = int(input())
+buildings = list(map(int, input().split()))
 def slope(x1,x2,y1,y2):
     return (y1-y2)/(x1-x2)
-
-buildings = list(map(int, input().split()))
 answer = 0
-for (index, building) in enumerate(buildings):
+for idx, height in enumerate(buildings):
     temp_answer = 0
-    # 오른쪽 체크하기
-    current_x = index
-    current_y = building
+    current_index = idx
+    current_height = height
+
     right_slope = -int(1e9)
-    for i in range(index+1, building_count):
-        temp_x = i
-        temp_y = buildings[i]
-        temp_slope = slope(current_x, temp_x, current_y, temp_y)
-        if temp_slope > right_slope:
+    for right in range(idx+1, buildings_count):
+        right_index = right
+        right_height = buildings[right]
+        temp_slope = slope(current_index, right_index, current_height, right_height)
+        if right_slope < temp_slope:
             temp_answer += 1
             right_slope = temp_slope
 
-
-    # 왼쪽 체크하기
     left_slope = int(1e9)
-    for i in range(index-1, -1, -1):
-        temp_x = i
-        temp_y = buildings[i]
-        temp_slope = slope(current_x, temp_x, current_y, temp_y)
-
-        if temp_slope < left_slope:
+    for left in range(idx-1, -1, -1):
+        left_index = left
+        left_height = buildings[left]
+        temp_slope = slope(current_index, left_index, current_height, left_height)
+        if left_slope > temp_slope:
             temp_answer += 1
             left_slope = temp_slope
-
-    answer = max(temp_answer, answer)
-
+    answer = max(answer, temp_answer)
 print(answer)
+
+
