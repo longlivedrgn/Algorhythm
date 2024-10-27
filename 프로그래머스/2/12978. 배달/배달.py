f29d83aaf8cf2045f7cecc66dd344c0c52dce3ec
Yongjae Kim <85781941+longlivedrgn@users.noreply.github.com>
2024-10-27 12:56:14 +0900

    [level 2] Title: 배달, Time: 1.99 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -3,32 +3,31 @@ import heapq
 def solution(N, road, K):
     INF = int(1e9)
     graph = [[] for _ in range(N+1)]
-    distance = [INF] * (N+1)
-    answer = 0
-    
+    distance = [INF for _ in range(N+1)]
+    answer = 0 
     for r in road:
-        start = r[0]
-        end = r[1]
-        cost = r[2]
-        graph[start].append((end, cost))
-        graph[end].append((start, cost))
+        graph[r[0]].append((r[1], r[2]))
+        graph[r[1]].append((r[0], r[2]))
     
-    q = []
-    heapq.heappush(q, (0, 1))
-    distance[1] = 0
-    
-    while q:
-        dist, now = heapq.heappop(q)
+    def dij(start):
+        # global graph, distance
+        q = []
+        heapq.heappush(q, (0, start))
+        distance[start] = 0
         
-        if dist <= distance[now]:
-            for i in graph[now]:
-                cost = dist + i[1]
-                
-                if cost < distance[i[0]]:
-                    distance[i[0]] = cost
-                    heapq.heappush(q, (cost, i[0]))
-                    
+        while q:
+            dist, now = heapq.heappop(q)
+            if dist <= distance[now]:
+                for i in graph[now]:
+                    cost = dist + i[1]
+                    if cost < distance[i[0]]:
+                        distance[i[0]] = cost
+                        heapq.heappush(q, (cost, i[0]))
+    dij(1)
     for d in distance:
         if d <= K:
             answer += 1
     return answer
+            
+        
+        
