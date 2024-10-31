2360e594a191c234864951ca1123547449983f49
Yongjae Kim <85781941+longlivedrgn@users.noreply.github.com>
2024-10-31 17:20:51 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 5.16 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,4 +1,8 @@
 def solution(files):
+    # 중요한 포인트!...
+    # 먼저 파일명을 head, number, tail로 split하는거!.. 그걸 어떤 식으로 할 지(flag 활용)
+    # 그리고 그걸 다시 리스트로 묶어서 람다함수를 활용해서 sorting하기!..
+    # sorting하는 방법 꼭꼭 기억하기!.. 
     answer = []
     splitted = []
     for file in files:
