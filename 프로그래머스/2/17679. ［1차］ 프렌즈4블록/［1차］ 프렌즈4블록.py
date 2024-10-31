b904cbb77f1ca46a8947504b381e08eeb778191f
Yongjae Kim <85781941+longlivedrgn@users.noreply.github.com>
2024-10-31 17:09:38 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 246.91 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -2,43 +2,46 @@ dr = [1, 1, 0]
 dc = [0, 1, 1]
 
 def solution(m, n, board): # M -> row, N -> column 
+    boards = []
     answer = 0
-    rboard = [list(i) for i in board]
-    # "0"으로 바꾸기
+    row = m
+    column = n
+    
+    for b in board:
+        boards.append(list(b))
+        
     while True:
-        delete_list = []
-        temp_answer = 0
-        for r in range(m):
-            for c in range(n):
-                if rboard[r][c] != "0":
-                    temp_list = [[r, c]]
+        delete_list = set()
+        for r in range(row):
+            for c in range(column):
+                now = boards[r][c]
+                if now != " ":
+                    temp_list = [(r, c)]
                     for i in range(3):
-                        nr = r+dr[i]
-                        nc = c + dc[i]
-
-                        if 0 <= nr < m and 0 <= nc < n:
-                            if rboard[nr][nc] == rboard[r][c]:
-                                temp_list.append([nr, nc])
+                        next_r = r + dr[i]
+                        next_c = c + dc[i]
+                        
+                        if 0 <= next_c < column and 0 <= next_r < row and boards[next_r][next_c] == now:
+                            temp_list.append((next_r, next_c))
                     if len(temp_list) == 4:
-                        delete_list.append(temp_list)
-        for delete in delete_list:
-            for d in delete:
-                if rboard[d[0]][d[1]] != "0":
-                    temp_answer += 1
-                    rboard[d[0]][d[1]] = "0"
-        answer += temp_answer
-        if temp_answer == 0:
+                        for temp in temp_list:
+                            delete_list.add(temp)
+        answer += len(delete_list)
+        if len(delete_list) != 0:
+            for d in delete_list:
+                boards[d[0]][d[1]] = " "
+        else:
             break
         
-        # 아래로 내리기
-        for r in range(m-1, -1, -1):
-            row_list = rboard[r]
-            for (c, value) in enumerate(row_list):
-                if value == "0":
-                    for i in range(r, -1, -1):
-                        for v in range(i, -1, -1):
-                            if rboard[v][c] != "0" and rboard[r][c] == "0":
-                                rboard[r][c] = rboard[v][c]
-                                rboard[v][c] = "0"
-            
+        for i in range(row-1, -1, -1):
+            temp_row = boards[i]
+            for (c, value) in enumerate(temp_row):
+                if value == " ":
+                    for t in range(i, -1, -1):
+                        if boards[t][c] != " ":
+                            boards[i][c] = boards[t][c]
+                            boards[t][c] = " "
+                            break
+                            
+
     return answer
