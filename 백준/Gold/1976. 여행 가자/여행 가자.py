a80063c0a65491d5440a093f1c44eac2f62af710
Yongjae Kim <85781941+longlivedrgn@users.noreply.github.com>
2024-10-31 20:19:30 +0900

    [Gold IV] Title: 여행 가자, Time: 192 ms, Memory: 114292 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -1,36 +1,35 @@
 from collections import deque
+
 cities_count = int(input())
-plan_cities_count = int(input())
+plan_count = int(input())
 cities = []
 for _ in range(cities_count):
     cities.append(list(map(int, input().split())))
-plan_cities = list(map(int, input().split()))
+plans = list(map(int, input().split()))
 
-def BFS(start, end):
+
+def BFS(start, end): # 1, 2
     visited = [False for _ in range(cities_count)]
-
-    queue = deque()
     visited[start-1] = True
-    queue.append(start)
+    q = deque()
+    q.append(start-1)
+    isFound = False
+    while q:
+        popped = q.popleft()
+        if popped == end-1:
+            isFound = True
+            break
 
-    while queue:
-        popped = queue.popleft()
-
-        if popped == end:
-            return True
-
-        for (index,i) in enumerate(cities[popped-1]):
-            if i == 1 and visited[index] == False:
-                queue.append(index+1)
-                visited[index] = True
-    return False
+        for (idx,m) in enumerate(cities[popped]):
+            if m == 1 and visited[idx] == False:
+                visited[idx] = True
+                q.append(idx)
+    return isFound
 
 
-for i in range(0, plan_cities_count-1):
-    if not BFS(plan_cities[i], plan_cities[i+1]):
+for i in range(0, plan_count -1):
+    if not BFS(plans[i], plans[i+1]):
         print("NO")
         break
 else:
     print("YES")
-
-
